c5317c3e4dcc23a89b343af173b0bcbb5585d63a
jesuslopez20 <68919679+jesuslopez20@users.noreply.github.com>
2020-08-14 16:07:29 -0500

    ejercicio de condicionales en python

---
+++ ejercicio5C.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Digite un numero: "))
+numero2 = int(input("Digite otro numero: "))
+
+if numero1 < numero2:
+    print(f"numero 1: {numero1}")
+    print(f"numero 2: {numero2}")
+
+else:
+    print(f"numero 1: {numero2}")
+    print(f"numero 2: {numero1}")
+
